ceb0a1df744dc008494a629c28fd66c77232f5f7
emonBimantara <shelmondbimantara@gmail.com>
2026-06-14 14:27:23 +0700

    Fix: fix logout route

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
                 </h1>
 
                 <div className="flex items-center gap-2">
-                    <form action="api/logout" method="POST">
+                    <form action="/api/logout" method="POST">
                         <Button type="submit" className="bg-red-600 hover:bg-red-700 transition py-5 text-white text-md cursor-pointer">
                             Logout
                         </Button>
